7104c1da3378889ac92fe03f44417d30a3169217
axemonkey <hi+github@clivemurray.com>
2023-06-19 16:34:23 +0100

    tidying and reindenting, mostly

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,88 +4,85 @@ const checkPulse = () => {
 
 window.addEventListener('load', checkPulse);
 
-
-
 // from casper theme
 
 // sticky nav
 const initStickyNav = function(s, a) {
 
 	console.log('init sticky nav');
-    s.Casper || (s.Casper = {}), s.Casper.stickyNavTitle = function(e) {
-        var t = a.querySelector(e.navSelector),
-            i = a.querySelector(e.titleSelector),
-            r = s.scrollY,
-            n = !1;
+		s.Casper || (s.Casper = {}), s.Casper.stickyNavTitle = function(e) {
+			var t = a.querySelector(e.navSelector),
+				i = a.querySelector(e.titleSelector),
+				r = s.scrollY,
+				n = !1;
 
-        function o() {
-            i.getBoundingClientRect().top + s.scrollY + (i.offsetHeight + 35) <= r ? t.classList.add(e.activeClass) : t.classList.remove(e.activeClass), n = !1
-        }
-        s.addEventListener("scroll", function() {
-            r = s.scrollY,
-                function() {
-                    n || requestAnimationFrame(o);
-                    n = !0
-                }()
-        }, {
-            passive: !0
-        }), o()
-    }
+			function o() {
+				i.getBoundingClientRect().top + s.scrollY + (i.offsetHeight + 35) <= r ? t.classList.add(e.activeClass) : t.classList.remove(e.activeClass), n = !1
+			}
+			s.addEventListener("scroll", function() {
+				r = s.scrollY,
+				function() {
+					n || requestAnimationFrame(o);
+					n = !0
+				}()
+			}, {
+				passive: !0
+			}), o()
+		}
 };
 
 const initHomepageNav = function() {
+	var nav = document.querySelector('.site-nav-main .site-nav');
+	var feed = document.querySelector('.post-feed');
 
-	        var nav = document.querySelector('.site-nav-main .site-nav');
-	        var feed = document.querySelector('.post-feed');
+	var lastScrollY = window.scrollY;
+	var lastWindowHeight = window.innerHeight;
+	var lastDocumentHeight = getDocumentHeight();
+	var ticking = false;
 
-	        var lastScrollY = window.scrollY;
-	        var lastWindowHeight = window.innerHeight;
-	        var lastDocumentHeight = getDocumentHeight();
-	        var ticking = false;
+	function getDocumentHeight() {
+		var cs = getComputedStyle(document.querySelector('body'));
+		var csh = cs.height;
+		console.log(csh);
+		return csh;
+	}
 
-	        function getDocumentHeight() {
-	        	var cs = getComputedStyle(document.querySelector('body'));
-	        	var csh = cs.height;
-	        	console.log(csh);
-	        	return csh;
-	        }
+	function onScroll() {
+		lastScrollY = window.scrollY;
+		requestTick();
+	}
 
-	        function onScroll() {
-	            lastScrollY = window.scrollY;
-	            requestTick();
-	        }
+	function onResize() {
+		lastWindowHeight = window.innerHeight;
+		lastDocumentHeight = getDocumentHeight();
+		requestTick();
+	}
 
-	        function onResize() {
-	            lastWindowHeight = window.innerHeight;
-	            lastDocumentHeight = getDocumentHeight();
-	            requestTick();
-	        }
+	function requestTick() {
+		if (!ticking) {
+			requestAnimationFrame(update);
+		}
+		ticking = true;
+	}
 
-	        function requestTick() {
-	            if (!ticking) {
-	                requestAnimationFrame(update);
-	            }
-	            ticking = true;
-	        }
+	function update() {
+		var trigger = feed.getBoundingClientRect().top + window.scrollY;
+		var progressMax = lastDocumentHeight - lastWindowHeight;
 
-	        function update() {
-	            var trigger = feed.getBoundingClientRect().top + window.scrollY;
-	            var progressMax = lastDocumentHeight - lastWindowHeight;
+		// show/hide nav
+		if (lastScrollY >= trigger - 20) {
+			nav.classList.add('fixed-nav-active');
+		} else {
+			nav.classList.remove('fixed-nav-active');
+		}
 
-	            // show/hide nav
-	            if (lastScrollY >= trigger - 20) {
-	                nav.classList.add('fixed-nav-active');
-	            } else {
-	                nav.classList.remove('fixed-nav-active');
-	            }
+		ticking = false;
+	}
 
-	            ticking = false;
-	        }
+	window.addEventListener('scroll', onScroll, { passive: true });
+	window.addEventListener('resize', onResize, false);
 
-	        window.addEventListener('scroll', onScroll, { passive: true });
-	        window.addEventListener('resize', onResize, false);
-
-	        update();
+	update();
 
 };
 
@@ -96,9 +93,9 @@ window.addEventListener('load', function() {
 	} else if (bodyEl.classList.contains('post-template')) {
 		initStickyNav(window, document);
 		Casper.stickyNavTitle({
-		    navSelector: '.site-nav-main',
-		    titleSelector: '.post-full-title',
-		    activeClass: 'nav-post-title-active'
+			navSelector: '.site-nav-main',
+			titleSelector: '.post-full-title',
+			activeClass: 'nav-post-title-active'
 		});
 	}
 });
